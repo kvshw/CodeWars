596b49ea45fd0abd432e9ecf3b5f45745dd1c27f
kavishwa-bhashitha <kaveebhashiofficial@gmail.com>
2023-06-14 10:36:07 +0300

    Sixth kata added

---
+++ school-paperwork.js
@@ -0,0 +1,11 @@
+function paperwork(n, m) {
+  // if(n<0 || m<0){
+  //   return 0
+  // }
+  // else{
+  //   return n*m
+  // }
+
+  // returning blank pages
+  return n < 0 || m < 0 ? 0 : n * m;
+}
