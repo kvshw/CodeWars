5f70ddd618cde4c56b122a2ac591c82b00655914
kavishwa-bhashitha <kaveebhashiofficial@gmail.com>
2023-06-24 10:12:20 +0300

    26th kata added.

--- reverse-words.js
+++ reverse-words.js
@@ -3,3 +3,21 @@
 // Examples
 // "This is an example!" ==> "sihT si na !elpmaxe"
 // "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+  let splitArray = str.split(" ");
+  let reversedArray = [];
+
+  for (let i = 0; i < splitArray.length; i++) {
+    let word = splitArray[i];
+    let reversedWord = "";
+
+    for (let j = word.length - 1; j >= 0; j--) {
+      reversedWord += word[j];
+    }
+
+    reversedArray.push(reversedWord);
+  }
+
+  return reversedArray.join(" ");
+}
