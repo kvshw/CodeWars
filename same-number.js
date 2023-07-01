187ae746df31e144056ac1b32ee97c2b0737440f
kavishwa-bhashitha <kaveebhashiofficial@gmail.com>
2023-07-01 22:36:41 +0300

    37th kata added.

---
+++ same-number.js
@@ -0,0 +1,48 @@
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+// Examples
+// Valid arrays
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+// Invalid arrays
+// If, for example, we change the first number to something else, comp is not returning true anymore:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+// Remarks
+// a or b might be [] or {} (all languages except R, Shell).
+// a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+// If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+
+// Note for C
+// The two arrays have the same size (> 0) given as parameter in function comp.
+
+function comp(array1, array2) {
+  // Check if either array is null, empty, or undefined
+  if (!array1 || !array2 || array1.length !== array2.length) {
+    return false;
+  }
+
+  // Sort the arrays in ascending order
+  const sortedArray1 = array1.sort((a, b) => a - b);
+  const sortedArray2 = array2.sort((a, b) => a - b);
+
+  // Check if each element in sortedArray2 is the square of the corresponding element in sortedArray1
+  for (let i = 0; i < sortedArray1.length; i++) {
+    if (sortedArray1[i] * sortedArray1[i] == sortedArray2[i]) {
+      return true;
+    }
+  }
+
+  return false;
+}
