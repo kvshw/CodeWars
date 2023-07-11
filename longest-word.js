f17418142029f3eea0b197253ead37a9d284e4c2
kavishwa-bhashitha <kaveebhashiofficial@gmail.com>
2023-07-11 16:44:59 +0300

    49th kata added.

--- longest-word.js
+++ longest-word.js
@@ -1,4 +1,4 @@
-// Simple, given a string of words, return the length of the Shortest word(s).
+// Simple, given a string of words, return the length of the Longest word(s).
 
 // String will never be empty and you do not need to account for different data types.
 
@@ -6,15 +6,15 @@ function findShort(s) {
   //Split the Array at " "
   let splittedArray = s.split(" ");
   //Setting shortest as the first item in the splitted array
-  let shortest = splittedArray[0].length;
+  let longest = splittedArray[0].length;
 
   for (let i = 0; i < splittedArray.length; i++) {
     // if the length is shorter than shortest one it will be added as the shortest string
-    if (splittedArray[i].length < shortest) {
+    if (splittedArray[i].length > longest) {
       // replace the shortest
-      shortest = splittedArray[i].length;
+      longest = splittedArray[i].length;
     }
   }
   // return shortest
-  return shortest;
+  return longest;
 }
